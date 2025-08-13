1b0afd97b50ec66f9d462d7f70b60bb06b035002
Sandeep <sandeepsharma78784@gmail.com>
2025-08-13 10:36:07 +0530

    Added custom directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { HighlightDirective } from './highlight.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,EmployeeListComponent],
+  imports: [RouterOutlet,EmployeeListComponent,HighlightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
